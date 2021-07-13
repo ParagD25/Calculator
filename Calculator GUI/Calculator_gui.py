16072487747c5ca3a53eb2257895cf339e3f2f35
Parag <paragdurafe@outlook.com>
2021-07-13 09:45:35 +0530

    Button funtionality added

--- Calculator GUI/Calculator_gui.py
+++ Calculator GUI/Calculator_gui.py
@@ -2,35 +2,85 @@ from tkinter import *
 window=Tk()
 window.title('Simple Calculator')
 display=Entry(window,width=11,bg='#242424',font=("Calibri",42),justify="right",fg='white')
-# display=Entry(window,font=("Calibri",12),justify="center",width=6,bg="#1E6FBA",fg="yellow",disabledbackground="#1E6FBA",disabledforeground="yellow",highlightbackground="black",highlightcolor="red",highlightthickness=1,bd=0)
 
 display.grid(row=0,column=0,rowspan=2,columnspan=4)
 # display_label=Label(window,width=45,height=7,bg='#242424')
 # display_label.grid(row=0,column=0,rowspan=2,columnspan=4)
 
 def insert_num(num):
-    pass
+    current=display.get()
+    display.delete(0,END)
+    display.insert(0,str(current)+str(num))
+
+def clear():
+    display.delete(0,END)
+
+def add():
+    first=display.get()
+    global fnum
+    global operation
+    operation='addition'
+    fnum=int(first)
+    display.delete(0,END)
+
+def sub():
+    first=display.get()
+    global fnum
+    global operation
+    operation='subtraction'
+    fnum=int(first)
+    display.delete(0,END)
+
+def mult():
+    first=display.get()
+    global fnum
+    global operation
+    operation='multiplication'
+    fnum=int(first)
+    display.delete(0,END)
+
+def div():
+    first=display.get()
+    global fnum
+    global operation
+    operation='division'
+    fnum=int(first)
+    display.delete(0,END)
+
+def equal():
+    new_num=display.get()
+    display.delete(0,END)
+    if operation=='addition':
+        display.insert(0,fnum+int(new_num))
+    if operation=='subtraction':
+        display.insert(0,fnum-int(new_num))
+    if operation=='multiplication':
+        display.insert(0,fnum*int(new_num))
+    if operation=='division':
+        display.insert(0,fnum/int(new_num))
+
+
 #Defining Button
-button_1=Button(window,text='1',width=10,height=3,bg='#070707',fg='white') 
-button_2=Button(window,text='2',width=10,height=3,bg='#070707',fg='white')
-button_3=Button(window,text='3',width=10,height=3,bg='#070707',fg='white')
-button_4=Button(window,text='4',width=10,height=3,bg='#070707',fg='white')
-button_5=Button(window,text='5',width=10,height=3,bg='#070707',fg='white')
-button_6=Button(window,text='6',width=10,height=3,bg='#070707',fg='white')
-button_7=Button(window,text='7',width=10,height=3,bg='#070707',fg='white')
-button_8=Button(window,text='8',width=10,height=3,bg='#070707',fg='white')
-button_9=Button(window,text='9',width=10,height=3,bg='#070707',fg='white')
-button_0=Button(window,text='0',width=10,height=3,bg='#070707',fg='white')
-button_add=Button(window,text='+',width=10,height=3,bg='#161616',fg='white')
-button_sub=Button(window,text='-',width=10,height=3,bg='#161616',fg='white')
-button_mult=Button(window,text='X',width=10,height=3,bg='#161616',fg='white')
-button_div=Button(window,text='/',width=10,height=3,bg='#161616',fg='white')
-button_equal=Button(window,text='=',width=10,height=3,bg='#15456b',fg='white')
+button_1=Button(window,text='1',width=10,height=3,bg='#070707',fg='white',command=lambda:insert_num(1)) 
+button_2=Button(window,text='2',width=10,height=3,bg='#070707',fg='white',command=lambda:insert_num(2))
+button_3=Button(window,text='3',width=10,height=3,bg='#070707',fg='white',command=lambda:insert_num(3))
+button_4=Button(window,text='4',width=10,height=3,bg='#070707',fg='white',command=lambda:insert_num(4))
+button_5=Button(window,text='5',width=10,height=3,bg='#070707',fg='white',command=lambda:insert_num(5))
+button_6=Button(window,text='6',width=10,height=3,bg='#070707',fg='white',command=lambda:insert_num(6))
+button_7=Button(window,text='7',width=10,height=3,bg='#070707',fg='white',command=lambda:insert_num(7))
+button_8=Button(window,text='8',width=10,height=3,bg='#070707',fg='white',command=lambda:insert_num(8))
+button_9=Button(window,text='9',width=10,height=3,bg='#070707',fg='white',command=lambda:insert_num(9))
+button_0=Button(window,text='0',width=10,height=3,bg='#070707',fg='white',command=lambda:insert_num(0))
+button_add=Button(window,text='+',width=10,height=3,bg='#161616',fg='white',command=add)
+button_sub=Button(window,text='-',width=10,height=3,bg='#161616',fg='white',command=sub)
+button_mult=Button(window,text='X',width=10,height=3,bg='#161616',fg='white',command=mult)
+button_div=Button(window,text='/',width=10,height=3,bg='#161616',fg='white',command=div)
+button_equal=Button(window,text='=',width=10,height=3,bg='#15456b',fg='white',command=equal)
 button_pos_neg=Button(window,text='+/-',width=10,height=3,bg='#070707',fg='white')
 button_dot=Button(window,text='.',width=10,height=3,bg='#070707',fg='white')
 button_per=Button(window,text='%',width=10,height=3,bg='#161616',fg='white')
 button_clear1=Button(window,text='CE',width=10,height=3,bg='#161616',fg='white')
-button_clear2=Button(window,text='C',width=10,height=3,bg='#161616',fg='white')
+button_clear2=Button(window,text='C',width=10,height=3,bg='#161616',fg='white',command=clear)
 button_back=Button(window,text='<]',width=10,height=3,bg='#161616',fg='white')
 button_onebyx=Button(window,text='1/x',width=10,height=3,bg='#161616',fg='white')
 button_sq=Button(window,text='x^2',width=10,height=3,bg='#161616',fg='white')
